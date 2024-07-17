cd7756e6f90a154d322787636c3f58ab2b80b0dd
josh.wahome <josh.kidfileapp@gmail.com>
2024-07-17 12:16:30 -0400

    image to be build

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,22 @@
 # Use an official Ubuntu as a parent image
 FROM ubuntu:latest
 
+# Install necessary packages
+RUN apt-get update && apt-get install -y \
+    apt-utils \
+    sudo \
+    curl \
+    && rm -rf /var/lib/apt/lists/*
+
 # Set the working directory
 WORKDIR /app
 
 # Copy the script into the container at /app
-COPY configuration2.sh /app/configuration2.sh
+ADD configuration2.sh /configuration2.sh
 
-# Run the script
-RUN chmod +x /app/configuration2.sh && /app/configuration2.sh
+# Make the script executable
+RUN chmod +x /configuration2.sh
 
 # Define the command to run the script
-CMD ["/bin/bash", "/app/configuration2.sh"]
+ENTRYPOINT ["./configuration2.sh"]
+
--- build-image.sh
+++ build-image.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
 # Update packages and install security patches
-sudo apt-get update
-sudo apt-get upgrade -y
+sudo yum update -y
 
 # Install Docker
-sudo apt-get install -y docker.io
+sudo amazon-linux-extras install docker -y
 
 # Start Docker daemon
-sudo systemctl start docker
+sudo service docker start
 
 # Enable Docker daemon to start at boot
 sudo systemctl enable docker
 
+# Add ec2-user to the docker group so you can execute Docker commands without using sudo
+sudo usermod -a -G docker ec2-user
+
 # Build the Docker image
-docker build -t king-httpd .
+docker build -t king-httpd-2 .
 
 # Tag the image
-docker tag king-httpd josh1991/king-httpd
+docker tag king-httpd-2 josh1991/king-httpd-2
 
 # Login to Docker Hub using environment variables
 echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin
 
 # Push the image to Docker Hub
-docker push josh1991/king-httpd
+docker push josh1991/king-httpd-2
